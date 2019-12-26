bdb1977cd650c5123c2dcd72679aa3080e78f3fa
sonicat <david.sue@gmail.com>
2019-12-26 08:44:59 -0800

    Add financialsv2

--- pipeline_live/data/polygon/fundamentals_loader.py
+++ pipeline_live/data/polygon/fundamentals_loader.py
@@ -24,7 +24,7 @@ class PolygonFinancialsLoader(PipelineLoader):
 
     def load_adjusted_array(self, domain, columns, dates, sids, mask):
 
-        financials = polygon.financials()
+        financials = polygon.financialsv2()
         out = {}
         for c in columns:
             data = [
